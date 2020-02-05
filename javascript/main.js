5b112a3d07e4053bb13235b9c10533a1bd7b8d22
Edward <pizzaman9272@gmail.com>
2020-02-05 11:18:35 -0500

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,1 @@
-function mouse_position() {
-    var e = window.event;
 
-    var posX = e.clientX;
-    var posY = e.clientY;
-
-    document.Form1.posx.value = posX;
-    document.Form1.posy.value = posY;
-
-    var t = setTimeout(mouse_position,100);
-}
