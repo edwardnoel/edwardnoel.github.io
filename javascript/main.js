9e93e81e64eb66e2418ca2b27cbd569e2e3aae7b
Edward <pizzaman9272@gmail.com>
2020-02-05 11:12:50 -0500

    Trying mouse position updater

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,12 @@
+function mouse_position() {
+    var event = window.event;
 
+    var x = event.clientX;
+    var y = event.clientY;
+
+    document.Form1.x.value = x;
+    document.Form1.y.value = y;
+
+    var t = setTimeout(mouse_position,100);
+
+}
